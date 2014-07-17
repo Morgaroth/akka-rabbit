d3f8f77805c3a4617d3aef9a3465d89ee53c5eb1
pjan vandaele <pjan@pjan.io>
2014-07-17 17:24:51 +0900

    Updated examples to fit the Refactor

--- akka-rabbit-example/src/main/scala/com/coiney/akka/rabbit/example/ConsumerExample1.scala
+++ akka-rabbit-example/src/main/scala/com/coiney/akka/rabbit/example/ConsumerExample1.scala
@@ -1,9 +1,8 @@
 package com.coiney.akka.rabbit.example
 
 import akka.actor.{Props, ActorSystem}
-import com.typesafe.config.ConfigFactory
 
-import com.coiney.akka.rabbit.{QueueConfig, RabbitFactory}
+import com.coiney.akka.rabbit.{QueueConfig, RabbitSystem}
 import com.coiney.akka.rabbit.messages._
 
 
@@ -11,18 +10,20 @@ object ConsumerExample1 extends App {
 
   implicit val system = ActorSystem("ConsumerSystem")
 
-  // load the configuration and initialize the RabbitFactory
-  val cfg = ConfigFactory.load()
-  val rabbit = RabbitFactory(cfg)
+  val rabbitSystem = RabbitSystem()
 
   // create the connection keeper and wait for it to be connected
-  val connectionKeeper = rabbit.createConnection(Some("connection"))
-  rabbit.waitForConnection(connectionKeeper)
+  val connectionKeeper = rabbitSystem waitFor rabbitSystem.createConnection("connection")
+  // // alternatively, you can also
+  // val connectionKeeper = rabbitSystem.createConnection("connection")
+  // rabbitSystem.waitForConnection(connectionKeeper)
 
   // create the producer and wait for it to be connected
   val consumeActor = system.actorOf(Props(classOf[ConsumeActor]))
-  val consumer = rabbit.createConsumer(connectionKeeper, consumeActor, name = Some("consumer"))
-  rabbit.waitForConnection(consumer)
+  val consumer = rabbitSystem waitFor rabbitSystem.createConsumer(connectionKeeper, consumeActor, "consumer")
+  // // Same here: alternatively, you can also
+  // val consumer = rabbitSystem.createConsumer(connectionKeeper, consumeActor, "consumer")
+  // rabbitSystem.waitForConnection(consumer)
 
   // consume the queue
   consumer ! ConsumeQueue(QueueConfig("my_queue"))
--- akka-rabbit-example/src/main/scala/com/coiney/akka/rabbit/example/RPCServerExample.scala
+++ akka-rabbit-example/src/main/scala/com/coiney/akka/rabbit/example/RPCServerExample.scala
@@ -1,7 +1,6 @@
 package com.coiney.akka.rabbit.example
 
 import akka.actor.ActorSystem
-import com.typesafe.config.ConfigFactory
 
 import com.coiney.akka.rabbit.RPC.Result
 import com.coiney.akka.rabbit._
@@ -29,16 +28,12 @@ object RPCServerExample extends App {
   // Add system shutdown hook
   sys.addShutdownHook(system.shutdown())
 
-  // load the configuration and initialize the RabbitFactory
-  val cfg = ConfigFactory.load()
-  val rabbit = RabbitFactory(cfg)
+  val rabbitSystem = RabbitSystem()
 
   // create the connection keeper and wait for it to be connected
-  val connectionKeeper = rabbit.createConnection(Some("connection"))
-  rabbit.waitForConnection(connectionKeeper)
+  val connectionKeeper = rabbitSystem waitFor rabbitSystem.createConnection("connection")
 
   // create the RPC Server and wait for it to be connected
-  val rpcServer = rabbit.createRPCServer(connectionKeeper, new ExclamationProcessor(), queueConfig = Some(QueueConfig("my_queue", durable = false, exclusive = false, autoDelete = true)))
-  rabbit.waitForConnection(rpcServer)
+  val rpcServer = rabbitSystem waitFor rabbitSystem.createRPCServer(connectionKeeper, new ExclamationProcessor(), QueueConfig("my_queue", durable = false, exclusive = false, autoDelete = true), "rpc-server")
 
 }
